91e6374da316cae615cef7a20e5a672cb93c070e
moha880 <moha@github.com>
2023-09-21 15:22:35 +0300

    Assignment1 Completed

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+arg1=$1
+
+arg2=$2
+
+if [ ! -d $arg1  ];then
+	echo "the path : "$arg1" is not specified"
+	exit 1
+fi
+
+
+if [ ! -n $arg2  ];then
+        echo "the : "$arg2" is not specified"
+        exit 1
+fi
+
+hhh=$(ls $arg1 | wc -l)
+
+ggg=$( grep -rni $arg2 $arg1 | wc -l)
+
+
+echo "The number of files are "$hhh" and the number of matching lines are "$ggg
---
+++ finder-app/writer.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+arg1=$1
+
+arg2=$2
+
+if [ ! -n $arg1  ];then
+        echo "the path : "$arg1" is not specified arg1 writer"
+        exit 1
+fi
+
+
+if [ ! -n $arg2  ];then
+        echo "the : "$arg2" is not specified arg2 writer"
+        exit 1
+fi
+
+DIR="$(dirname "${arg1}")"
+
+if [ ! -d $DIR  ];then
+        mkdir $DIR
+fi
+
+
+echo ${arg2} > ${arg1}
+
+if [ -f ${arg1} ];then
+	exit 0
+else
+	echo "ERROR : CREATING THE FILE"
+	exit 1
+fi
+
